84e249b3449efb71d022730141acb00e68b12215
Varun Upadhayay <66673166+VarunUpadhayay@users.noreply.github.com>
2021-12-05 13:46:06 +0530

    Update main.js

--- main.js
+++ main.js
@@ -11,13 +11,10 @@
     autoplay:true
     })
 
-    /*------------------
-        CountDown
-    --------------------*/
-    // For demo preview start
+    
     var today = new Date();
     var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); 
     var yyyy = today.getFullYear();
 
     if(mm == 12) {
@@ -28,11 +25,7 @@
         mm = String(mm).padStart(2, '0');
     }
     var timerdate = mm + '/' + dd + '/' + yyyy;
-    // For demo preview end
-
-    // Uncomment below and use your date //
-
-    /* var timerdate = "2020/12/30" */
+    
 
     jQuery("#countdown-time").countdown(timerdate, function(event) {
         jQuery(this).html(event.strftime("<div class='countdown_item'><span>%D</span> <p>Day</p> </div>" + "<div class='countdown_item'><span>%H</span> <p>Hour</p> </div>" + "<div class='countdown_item'><span>%M</span> <p>Min</p> </div>" + "<div class='countdown_item'><span>%S</span> <p>Sec</p> </div>"));
